623776626221bb0a8cabf5caca627a3d04e804eb
novimaulidiah <novi.maulidiah.2405366@students.um.ac.id>
2025-12-05 08:18:48 +0700

    Update analisis.h

--- analisis.h
+++ analisis.h
@@ -1,15 +1,8 @@
 #ifndef ANALISIS_H
 #define ANALISIS_H
 
-#include "pembaca_file.h"
-#include <map>
-
-void hitungTotalPerTahun(const std::vector<DataUMKM>& data,
-                         std::map<int, double>& total);
-
+double hitungTotal(double data[], int jumlah);
+double hitungRataRata(double data[], int jumlah);
 double hitungPertumbuhan(double awal, double akhir);
-
-void hitungKenaikanKategori(const std::vector<DataUMKM>& data,
-                            std::map<std::string, double>& kenaikan);
 
 #endif
